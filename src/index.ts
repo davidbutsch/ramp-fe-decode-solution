1dc05b520a5b30c8bd071757e91cb2f7364f79e5
David <102764748+davidbutsch@users.noreply.github.com>
2025-08-02 16:24:51 -0500

    get html content from challenge url

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,15 @@ dotenv.config();
 
 const challengeUrl = process.env.CHALLENGE_URL!;
 
-console.log(challengeUrl);
+const getChallengeHtml = async () => {
+  const response = await fetch(challengeUrl);
+  const html = await response.text();
+
+  return html;
+};
+
+(async () => {
+  const html = await getChallengeHtml();
+
+  console.log(html);
+})();
